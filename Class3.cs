40ad210bf12eb4edb658d3594e62c160af02f39e
funatsu-215 <daifukusan215@gmail.com>
2019-07-25 17:14:08 +0900

    second commit

---
+++ Class3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Keisan1
+    {
+        protected int jibun;
+        protected int aite;
+        protected int sasihiki;
+
+        public int Jibun
+        {
+            set
+            {
+                jibun = value;
+            }
+            get
+            {
+                return jibun;
+            }
+        }
+        public int Aite
+        {
+            set
+            {
+                aite = value;
+            }
+            get
+            {
+                return aite;
+            }
+        }
+        public int Sasihiki()
+        {
+            sasihiki = jibun - aite;
+            return sasihiki;
+          
+        }
+        public void Method()
+        {
+            Console.WriteLine("クラスでの差し引き値　{0}", sasihiki);
+        }
+        public void Kekka()
+        {
+            Console.WriteLine("自分{0}　-　相手{1} =　結果{2}",jibun,aite,jibun-aite);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keisan = new Keisan1();
+
+            //Random aite = new System.Random();
+            keisan.Aite = 1; //aite.Next(1, 4);
+                
+            Console.WriteLine("どれを出す？");
+            Console.WriteLine("1:グー\t2:チョキ\t3:パー");
+            keisan.Jibun = int.Parse(Console.ReadLine());
+
+            //相手の手を表示
+            Console.WriteLine("相手の手 \t {0}", keisan.Aite);
+            keisan.Sasihiki();
+            //計算結果確認用
+            keisan.Kekka();
+            //return確認（クラス）
+            keisan.Method();
+
+            //勝敗結果確認（継承先から呼び出し）
+            var shouhai = new Shouhai1();
+            shouhai.Shouhai2();
+
+            shouhai.Kakunin();
+        }
+    }
+}
