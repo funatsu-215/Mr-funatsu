3479ca8aab461c9b9cad2c5ad4a6223e04ee6af5
funatsu-215 <daifukusan215@gmail.com>
2019-08-01 17:27:20 +0900

    OJT:janken

---
+++ janken/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cpu人数入力
+            Console.WriteLine("cpu人数");
+            int cpuninzu = int.Parse(Console.ReadLine());
+            //player人数入力
+            Console.WriteLine("player人数");
+            int playerninzu = int.Parse(Console.ReadLine());
+            //回数入力
+            Console.WriteLine("回数");
+            int kaisu = int.Parse(Console.ReadLine());
+            //クラス作成:コンストラクタ値渡し
+            var keisan = new Class1(playerninzu,cpuninzu,kaisu);
+            //継承クラス作成：コンストラクタ値渡し
+            var shouhai = new Keishou1(playerninzu,cpuninzu,kaisu);
+
+            //乱数初期化
+            var ransu = new System.Random();
+
+            for (int k = 1; k <= kaisu; k++)
+            {
+                Console.WriteLine("{0}回目", k);
+                //あいこの時継続させる:それ以外は抜け出す
+                while (shouhai.Zokkou == 0)
+                {
+                    //cpuの手決定：乱数
+                    for (int i = 0; i < cpuninzu; i++)
+                    {
+                        keisan.cpu[i] = ransu.Next(0, 3);
+                        shouhai.cpu[i] = keisan.cpu[i];
+                    }
+                    //cpuの手:内容開示
+                    foreach (int nakami in keisan.cpu)
+                    {
+                        Console.WriteLine("{0}", nakami);
+                    }
+                    //playerの手決定:入力
+                    for (int i = 0; i < playerninzu; i++)
+                    {
+                        Console.WriteLine("Player{0}は何を出す？数字で選んでね", i);
+                        Console.WriteLine("0:グー　1:チョキ　2:パー");
+                        keisan.player[i] = int.Parse(Console.ReadLine());
+                        shouhai.player[i] = keisan.player[i];
+                    }
+                    //Cpuの手を表示
+                    keisan.Cpute();
+                    //Playerの手を表示
+                    keisan.Playerte();
+                    //結果表示
+                    shouhai.Kekka();
+                    //ビット演算初期化
+                    shouhai.kekka = 0;
+                }
+                shouhai.Zokkou = 0;
+                foreach(int nakami in shouhai.pcount)
+                {
+                    Console.WriteLine("pcountの中身　{0}", nakami);
+                }
+                foreach(int nakami in shouhai.ccount)
+                {
+                    Console.WriteLine("ccountの中身　{0}", nakami);
+                }
+            }
+            shouhai.Pshouritsu();
+            shouhai.Cshouritsu();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ janken/shouhai.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Keishou1 : Class1
+    {
+        public int zokkou = 0;
+        public int[] pcount;
+        public int[] ccount;
+        public float[] pshouritsu;
+        public float[] cshouritsu;
+        public Keishou1(int playerninzu, int cpuninzu, int kaisu) : base(playerninzu,cpuninzu,kaisu)
+        {
+            pshouritsu = new float[playerninzu];
+            cshouritsu = new float[cpuninzu];
+            pcount = new int[playerninzu];
+            ccount = new int[cpuninzu];
+        }
+        public int Zokkou
+        {
+            set { zokkou = value; }
+            get { return zokkou; }
+        }
+        public void Kekka()
+        {
+            for (int i = 0; i < player.Length; i++)
+            {
+                kekka = kekka | (1 << player[i]);
+            }
+            for (int i = 0; i < cpu.Length; i++)
+            {
+                kekka = kekka | (1 << cpu[i]);
+            }
+
+            if (kekka == 1 || kekka == 2 || kekka == 4 || kekka == 7)
+            {
+                Console.WriteLine("あいこだよ");
+                zokkou = 0;
+            }
+            else if (kekka == 6)
+            {
+                Console.WriteLine("チョキの勝ち");
+                for (int i = 0; i < player.Length; i++)
+                {
+                    if (player[i] == 1)
+                    {
+                        Console.WriteLine("PLAYER[{0}]の勝ち", i);
+                        pcount[i] += 1;
+                    }
+                    else if (player[i] == 0 || player[i] == 2)
+                    {
+                        pcount[i] += 0;
+                    }
+                }
+                for (int i = 0; i < cpu.Length; i++)
+                {
+                    if (cpu[i] == 1)
+                    {
+                        Console.WriteLine("CPU[{0}]の勝ち", i);
+                        ccount[i] += 1;
+                    }
+                    else if (cpu[i] == 0 || cpu[i] == 2)
+                    {
+                        ccount[i] += 0;
+                    }
+                }
+                zokkou = 1;
+            }
+            else if (kekka == 5)
+            {
+                Console.WriteLine("パーの勝ち");
+                for (int i = 0; i < player.Length; i++)
+                {
+                    if (player[i] == 2)
+                    {
+                        Console.WriteLine("PLAYER[{0}]の勝ち", i);
+                        pcount[i] += 1;
+                    }
+                    else if (player[i] == 0 || player[i] == 2)
+                    {
+                        pcount[i] += 0;
+                    }
+                }
+                for (int i = 0; i < cpu.Length; i++)
+                {
+                    if (cpu[i] == 2)
+                    {
+                        Console.WriteLine("CPU[{0}]の勝ち", i);
+                        ccount[i] += 1;
+                    }
+                    else if (cpu[i] == 0 || cpu[i] == 2)
+                    {
+                        ccount[i] += 0;
+                    }
+                }
+                zokkou = 1;
+            }
+            else if (kekka == 3)
+            {
+                Console.WriteLine("グーの勝ち");
+                for (int i = 0; i < player.Length; i++)
+                {
+                    if (player[i] == 0)
+                    {
+                        Console.WriteLine("PLAYER[{0}]の勝ち", i);
+                        pcount[i] += 1;
+                    }
+                    else if (player[i] == 0 || player[i] == 2)
+                    {
+                        pcount[i] += 0;
+                    }
+                }
+                for (int i = 0; i < cpu.Length; i++)
+                {
+                    if (cpu[i] == 0)
+                    {
+                        Console.WriteLine("CPU[{0}]の勝ち", i);
+                        ccount[i] += 1;
+                    }
+                    else if (cpu[i] == 0 || cpu[i] == 2)
+                    {
+                        ccount[i] += 0;
+                    }
+                }
+                zokkou = 1;
+            }
+            else
+            {
+                Console.WriteLine("エラーが起きてるよ");
+                Console.WriteLine("kekka{0}", kekka);
+                zokkou = 2;
+            }
+
+        }
+        public void Pshouritsu()
+        {
+            for (int i = 0; i < player.Length; i++)
+            {
+                pshouritsu[i] = (100 * pcount[i]) / kaisu;
+                Console.WriteLine("PLAYER{0}　勝率{1}%", i, pshouritsu[i]);
+            }
+        }
+        public void Cshouritsu()
+        {
+            for (int i = 0; i < cpu.Length; i++)
+            {
+                cshouritsu[i] = (100 * ccount[i]) / kaisu;
+                Console.WriteLine("CPU{0}　勝率{1}%", i, cshouritsu[i]);
+            }
+        }
+    }
+}
